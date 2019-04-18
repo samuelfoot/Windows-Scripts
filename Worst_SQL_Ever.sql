fafa8615df6f527397c5e2ac148d84bbb0d0cd2a
Samuel Foot <33418468+samuelfoot@users.noreply.github.com>
2019-04-18 16:30:49 +0100

    Update Worst_SQL_Ever.sql

--- Worst_SQL_Ever.sql
+++ Worst_SQL_Ever.sql
@@ -6,17 +6,17 @@ BEGIN
     if @rowcount_Table1=@rowcount_table2 then
 		    select 'Both tables have identical data';
     else
-		    SET @rowcount_Table1=@rowcount_Table1+1;
+        SET @rowcount_Table1=@rowcount_Table1+1;
         
-		    WHILE @rowcount_Table1 <= @rowcount_table2 DO
-
-				    set @createColumns =(select GROUP_CONCAT(CONCAT(q.sid, 'X', q.gid, 'X', q.qid, ' TEXT')) FROM lime_questions as q);
+        WHILE @rowcount_Table1 <= @rowcount_table2 DO
+            
+            set @createColumns =(select GROUP_CONCAT(CONCAT(q.sid, 'X', q.gid, 'X', q.qid, ' TEXT')) FROM lime_questions as q);
             SET @qry=CONCAT('create table resp_tmp (', @createColumns ,');');
-						PREPARE `stmt` FROM @`qry`;
+            PREPARE `stmt` FROM @`qry`;
             EXECUTE `stmt`;
             DEALLOCATE PREPARE `stmt`;
 						
-			      set @columnsQuestions =(select GROUP_CONCAT(CONCAT(q.sid, 'X', q.gid, 'X', q.qid)) FROM lime_questions as q);
+            set @columnsQuestions =(select GROUP_CONCAT(col ORDER BY col) from (select CONCAT(q.sid, 'X', q.gid, 'X', q.qid) as col FROM lime_questions as q order by col) as cols);
     
             SET @qry=CONCAT('insert into resp_tmp select ', @columnsQuestions ,' from lime_survey_785728 where id = ', @rowcount_Table1 ,';');
             PREPARE `stmt` FROM @`qry`;
@@ -29,7 +29,7 @@ BEGIN
             WHILE @counter <= @counterMAX DO
                 SET @rownr=0;
     
-                SET @qry=CONCAT('select col INTO @columname from (SELECT @rownr:=@rownr+1 AS rowNumber, CONCAT(q.sid, "X", q.gid, "X", q.qid) as col FROM lime_questions AS q order by col ) as cols where rowNumber = ', @counter ,';');
+                SET @qry=CONCAT('select col INTO @columname from (SELECT @rownr:=@rownr+1 AS rowNumber, CONCAT(q.sid, "X", q.gid, "X", q.qid) as col FROM lime_questions AS q ) as cols where rowNumber = ', @counter ,';');
                 PREPARE `stmt` FROM @`qry`;
                 EXECUTE `stmt`;
                 DEALLOCATE PREPARE `stmt`;
@@ -73,7 +73,7 @@ BEGIN
                 set @answer=(select IFNULL(@answer, " "));
                 
                 set @question=CONCAT('(SELECT q.question FROM lime_questions AS q where CONCAT(q.sid, "X", q.gid, "X", q.qid) = "', @columname, '")');
-                SET @qry=CONCAT('select CONCAT(', @question ,',": ","', @answer ,'") into @QandA;');
+                SET @qry=CONCAT('select CONCAT("<b>",', @question ,',"</b>: ","', @answer ,'") into @QandA;');
                 PREPARE `stmt` FROM @`qry`;
                 EXECUTE `stmt`;
                 DEALLOCATE PREPARE `stmt`;   
@@ -84,17 +84,17 @@ BEGIN
                 PREPARE `stmt` FROM @`qry`;
                 EXECUTE `stmt`;
                 DEALLOCATE PREPARE `stmt`;
-    		        SET @counter=@counter+1;
+				SET @counter=@counter+1;
             END WHILE;
             
-            SET @qry=CONCAT('select * INTO OUTFILE "/var/lib/mysql-files/', @rowcount_Table1 ,'.csv" FIELDS TERMINATED BY "\n" from resp_tmp;');
+            SET @qry=CONCAT('select * INTO OUTFILE "/var/lib/mysql-files/', @rowcount_Table1 ,'.csv" FIELDS TERMINATED BY " <br>" from resp_tmp;');
             PREPARE `stmt` FROM @`qry`;
             EXECUTE `stmt`;
             DEALLOCATE PREPARE `stmt`;
         
             SET @rowcount_Table1=@rowcount_Table1+1;
-						DROP TABLE resp_tmp;
-		    END WHILE;
+			DROP TABLE resp_tmp;
+		END WHILE;
         
     TRUNCATE TABLE rar_tmp;
     INSERT INTO rar_tmp select * from lime_survey_785728;
